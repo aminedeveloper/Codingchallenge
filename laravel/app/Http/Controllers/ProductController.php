54687a0e425eb4dd834a7293f5f3ab5e272beace
amine developer <facebookamine39@gmail.com>
2021-01-09 21:47:34 +0100

    create delete product function and the filtrage function
    + create product function
    + filter product function
    + delete product function

--- laravel/app/Http/Controllers/ProductController.php
+++ laravel/app/Http/Controllers/ProductController.php
@@ -11,7 +11,7 @@ class ProductController extends Controller
     public function allData(){
         $categories = Categorie::all();
         $products = Product::all();
-        return view('home', compact('categories', 'products'));
+        return view('Home', compact('categories', 'products'));
          
     }
 
@@ -24,21 +24,26 @@ class ProductController extends Controller
         $product->name = $request->input('productname');
         $product->description = $request->input('productdescription');
         $product->price = floatval($request->input('productprice'));
-        $file = $request->file('productimage')->getClientOriginalName();
+        $file = $request->file('productimage');
+
+        $input['productimage'] = time() .'.'.$file->getClientOriginalExtension();
+
+        $destinationpath=public_path('/productsimages');
+
+        $file->move($destinationpath , $input['productimage']);
          
  
         
-
-        Storage::disk('local')->put($file, 'Contents');
          
 
-        $product->image=$file;
+        $product->image=$input['productimage'];
 
         $product->category  = $request->input('category_parent_product');
 
         $product->save();
 
-        return View('/');
+        return redirect('/');
+
 
 
 
@@ -46,7 +51,66 @@ class ProductController extends Controller
 
     // This function below responsable to delete a product 
 
-    public function deleteProduct(){
+    public function deleteProduct(Request $request , $productid){
+
+            $product = Product::find($productid);
+
+            $product->delete();
+
+            return redirect('/');
+
+    }
+
+
+    // this function below responsable to filtering the products by name or price 
+
+    public function filterProduct(Request $request){
+
+        $filterby=$request->input('filterby');
+
+        switch ($filterby) {
+            case 'priceup':
+                $products = Product::select("*")
+                ->orderBy('price', 'asc')
+                ->get();
+                $categories = Categorie::all();
+
+                return view('Home', compact('categories', 'products'));
+                break;
+            
+            case 'pricedown':
+                $products = Product::select("*")
+                ->orderBy('price', 'desc')
+                ->get();
+                $categories = Categorie::all();
+
+                return view('Home', compact('categories', 'products'));
+                
+                break;
+
+            case 'name':
+                $products = Product::select("*")
+                ->orderBy('name')
+                ->get();
+                $categories = Categorie::all();
+
+                return view('Home', compact('categories', 'products'));
+                break;
+        }
+    }
+
+    // this function below responsable to filtering the product by name or price 
+
+    public function filterProductcategory(Request $request){
+        $filterby=$request->input('category_filter');
+        $categories = Categorie::all();
+
+        $products = Product::select("*")
+                            ->where('category',$filterby)
+                            ->get();
+        
+        return view('Home', compact('categories', 'products'));
+
 
     }
 }
